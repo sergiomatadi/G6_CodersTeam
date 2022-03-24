6845090196c9159d01777e916f13891c02c9f845
Inegreira <100797498+Inegreira@users.noreply.github.com>
2022-03-24 12:15:30 +0100

    Implementación de sacarDatos de localStorage

--- frontend/src/js/storage.js
+++ frontend/src/js/storage.js
@@ -1,12 +1,21 @@
-function obtenerEmail() {
+function obtenerDatos() {
+
+  //Espero a que el usuario clicke el boton para arrancar la funcion guardarDatos.
   let boton = document.getElementById("botonregistro");
-  boton.addEventListener("click", guardarMail, false);
+  boton.addEventListener("click", guardarDatos, false);
 }
 
-function guardarMail() {
+function guardarDatos() {
+
+  //Guardo los datos del usuario en el localStorage.
   let email = document.getElementById("inputEmail3").value;
   let user = document.getElementById("Username").value;
   localStorage.setItem(user, email);
+
+  //Obtengo el almacenamiento de localStorage en consola.
+  let guardarEmail = localStorage.getItem(email)
+  console.log(guardarEmail);
 }
 
-window.addEventListener("load", obtenerEmail, false);
+window.addEventListener("load", obtenerDatos, false);
+
