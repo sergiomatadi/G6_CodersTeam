ace427c04294db552951dea5956be36c32d76c29
denissemb <denissemb@gmail.com>
2022-03-26 00:25:57 +0100

    cambios sala2

--- frontend/src/js/salas.js
+++ frontend/src/js/salas.js
@@ -1,16 +1,21 @@
-function handleDragStart(e) {
-    this.style.opacity = '0.2';
+function dragstart(e) {
+    this.style.opacity = '0.1';
+
   }
   function allowDrop(ev) {
+    
     ev.preventDefault();
   }
   
   function drag(ev) {
+    ev.cursor ="pointer";
     ev.dataTransfer.setData("text", ev.target.id);
   }
   
   function drop(ev) {
+
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
+    ev.target.style.display = 'block';
   }
