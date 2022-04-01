27752676dc15c0c7114a0fadf464fc8827d9776d
Edwin Leal <edwinleal019@gmail.com>
2022-04-02 00:03:33 +0200

    front: post para guardar el avatar del user

--- frontend/src/js/avatar.js
+++ frontend/src/js/avatar.js
@@ -1,10 +1,29 @@
-function elegirAvatar(avatar) {
-  sessionStorage.avatarJugador = avatar;
-  window.location.href = "salas.html";
+const sesion = JSON.parse(localStorage.getItem("sesionUser"));
+async function elegirAvatar(avatar) {
+  // Guardamos en localStorage el user avatar
+  const updatedSesion = {
+    avatar,
+    ...sesion,
+  };
+  localStorage.setItem("sesionUser", JSON.stringify(updatedSesion));
+
+  const url = "http://localhost:3001/users/save_avatar";
+
+  const response = await fetch(`${url}?user=${sesion.email}`, {
+    method: "POST",
+    body: JSON.stringify({ avatar }),
+  });
+  const { statusCode } = await response.json();
+
+  if (statusCode === 200) {
+    window.location.href = "salas.html";
+    sessionStorage.avatarJugador = avatar;
+  } else {
+    console.error("Error saving user avatar");
+  }
 }
 
 const headerNameElement = document.getElementById("username");
-const sesion = JSON.parse(localStorage.getItem("sesionUser"));
 
 headerNameElement.insertAdjacentHTML(
   "beforeend",
