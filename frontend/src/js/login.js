03854921cd8a382c0cd4c30f09ca2db685682acc
Inegreira <100797498+Inegreira@users.noreply.github.com>
2022-04-01 09:50:45 +0200

    "Metodo para guardar avatar comentado"

--- frontend/src/js/login.js
+++ frontend/src/js/login.js
@@ -22,10 +22,29 @@ const checkUser = async () => {
   if (status === 200) {
     // Login OK redirigimos al user a las salas
     window.location.href = "avatar.html";
+    //llamada al método
+    // avatarElegido();
   } else {
     // Login FAIL deberiamos mostrar un mensaje al user diciendo' usuario o pass incorrecto'
     console.log("login fail", status);
   }
 };
+
+//Método para guardar en localStorage el avatar elegido en el login.
+//#Hace falta unirlo con el backend
+// function avatarElegido() {
+//   getElementById(avatar);
+//   if (login.avatar("avatar1")) {
+//     avatarElegido = "https://img.myloview.de/fototapeten/funny-cartoon-monster-face-vector-halloween-monster-square-avatar-700-176103253.jpg";
+//   } else if (login.avatar("avatar2")) {
+//     avatarElegido = "../img/ninja-removebg-preview.png";
+//   } else if (login.avatar("avatar3")) {
+//     avatarElegido = "../img/pirata-removebg-preview.png";
+//   } else if (login.avatar("avatar4")) {
+//     avatarElegido = "../img/pingu-removebg-preview.png";
+//   } else {
+//     avatarElegido = "https://img.myloview.de/fototapeten/funny-cartoon-monster-face-vector-halloween-monster-square-avatar-700-176103253.jpg";
+//   }
+// }
 
 window.addEventListener("load", logUser, false);
