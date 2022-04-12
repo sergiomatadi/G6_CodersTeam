ab24237f4c1108f312a0ec9953cbb34f15a8d314
jfontvit <jordifv90@gmail.com>
2022-04-12 09:22:25 +0200

    feat: instalacion de express y prueba

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,20 @@ const { createServer } = require("http");
 const users = require("./controllers/users"); // MODULO DE USERS
 const salas = require("./controllers/sala"); // MODULE SALAS
 
+// Express
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
 const PORT = 3001;
 
 const requestListener = (req, res) => {
