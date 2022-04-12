f6d0a834bf5c8747fb14a1f51c426568c1a80605
jfontvit <jordifv90@gmail.com>
2022-04-12 11:19:54 +0200

    docs: Añadida documentacion rutas iniciales

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,18 +9,22 @@ const path = require("path");
 const app = express();
 const PORT = 5000;
 
+// Para poder usar los CSS y JS e imagenes incrustados en el HTML
 app.use(express.static(path.join((__dirname, "../../frontend/src/"))));
 
+// Ruta principal de nuestra aplicacion
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../../frontend/src/html/", "home.html"));
 });
 
+// Ruta vista del formulario registro
 app.get("/register", (req, res) => {
   res.sendFile(
     path.join(__dirname, "../../frontend/src/html/", "register.html")
   );
 });
 
+// Ruta vista del formulario login
 app.get("/login", (req, res) => {
   res.sendFile(path.join(__dirname, "../../frontend/src/html/", "login.html"));
 });
