1fc84bf50ee3467e32833317930f2bad86e0ffa4
jfontvit <jordifv90@gmail.com>
2022-04-30 13:56:44 +0200

    feat: Creacion modelo y prueba del mismo usuario

--- backend/src/mongo.js
+++ backend/src/mongo.js
@@ -21,5 +21,27 @@ const userSchema = new mongoose.Schema({
   name: String,
   email: String,
   password: String,
-  avatar: undefined,
+  avatar: {},
 });
+
+// Modelo
+
+const User = mongoose.model("User", userSchema);
+
+const user = new User({
+  id: "123",
+  name: "Jordi",
+  email: "jordi@gmail.com",
+  password: "1234",
+  avatar: {},
+});
+
+user
+  .save()
+  .then((result) => {
+    console.log(result);
+    mongoose.connection.close();
+  })
+  .catch((err) => {
+    console.error(err);
+  });
