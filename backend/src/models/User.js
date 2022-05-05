b428efbf2ab89ddf28bb420d232c98bb9f88346b
Edwin Leal <edwinleal019@gmail.com>
2022-05-05 19:13:21 +0200

    modelo user actualizado

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -2,15 +2,35 @@ const mongoose = require("mongoose");
 
 // Schema
 const userSchema = new mongoose.Schema({
-  id: String,
-  name: String,
-  email: String,
-  password: String,
-  avatar: {},
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    dropDups: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+    default: null,
+  },
+});
+
+userSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id;
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
 });
 
 // Modelo
-
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
