6dcfc38aed32bc5400d0c8465d0dab834be15bac
Edwin Leal <edwinleal019@gmail.com>
2022-05-05 19:13:00 +0200

    comentarios borrados

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,7 @@
 // Mongodb
 require("./mongo");
-const User = require("./models/User");
 
 const users = require("./controllers/users"); // MODULO DE USERS
-const salas = require("./controllers/sala"); // MODULE SALAS
 const sockets = require("./controllers/sockets"); // MODULO SOCKET
 
 // Express
@@ -38,34 +36,26 @@ app.set("view engine", "pug");
 // Ruta principal de nuestra aplicacion
 app.get("/", (req, res) => {
   res.render("home");
-  // res.sendFile(path.join(__dirname, "../../frontend/src/html/", "home.html"));
 });
 
 // Ruta vista del formulario registro
 app.get("/register", (req, res) => {
   res.render("register");
-  //res.sendFile(
-  //  path.join(__dirname, "../../frontend/src/html/", "register.html")
-  //);
 });
 
 // Ruta vista del formulario login
 app.get("/login", (req, res) => {
   res.render("login");
-  // res.sendFile(path.join(__dirname, "../../frontend/src/html/", "login.html"));
 });
 
 app.get("/salas", (req, res) => {
   res.render("salas");
-  // res.sendFile(path.join(__dirname, "../../frontend/src/html/", "salas.html"));
 });
 
 app.get("/juego", (req, res) => {
   res.render("juego");
-  // res.sendFile(path.join(__dirname, "../../frontend/src/html/", "juego.html"));
 });
 
 app.get("/avatar", (req, res) => {
   res.render("avatar");
-  // res.sendFile(path.join(__dirname, "../../frontend/src/html/", "avatar.html"));
 });
